d174c5a1bb912620b033b686b31a432e407fbb26
YAHOR STRUPINSKI <88976947+EgorStrupinskiy@users.noreply.github.com>
2024-02-14 20:47:39 +0300

    Fast Category creation implemented

--- src/main/java/com/vention/agroex/util/mapper/ProductCategoryMapper.java
+++ src/main/java/com/vention/agroex/util/mapper/ProductCategoryMapper.java
@@ -1,4 +1,17 @@
 package com.vention.agroex.util.mapper;
 
+import com.vention.agroex.dto.ProductCategory;
+import com.vention.agroex.entity.ProductCategoryEntity;
+
+import java.util.List;
+
 public interface ProductCategoryMapper {
+
+    ProductCategoryEntity toEntity(ProductCategory category);
+
+    ProductCategory toDTO(ProductCategoryEntity entity);
+
+    List<ProductCategory> toDTOs(List<ProductCategoryEntity> entities);
+
+    List<ProductCategoryEntity> toEntities(List<ProductCategory> categories);
 }
