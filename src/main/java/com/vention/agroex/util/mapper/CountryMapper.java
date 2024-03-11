16eaff271883e826febf589b89b010d946788d90
alpha4vit <gurinovi4rm@gmail.com>
2024-03-11 22:32:36 +0300

    Return nested regions to country

--- src/main/java/com/vention/agroex/util/mapper/CountryMapper.java
+++ src/main/java/com/vention/agroex/util/mapper/CountryMapper.java
@@ -2,15 +2,18 @@ package com.vention.agroex.util.mapper;
 
 import com.vention.agroex.dto.Country;
 import com.vention.agroex.entity.CountryEntity;
+import com.vention.agroex.entity.LocationEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.List;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", imports = LocationEntity.class)
 public interface CountryMapper {
 
     CountryEntity toEntity(Country country);
 
+    @Mapping(target = "regions", expression = "java(countryEntity.getLocations().stream().map(LocationEntity::getRegion).toList())")
     Country toDTO(CountryEntity countryEntity);
 
     List<CountryEntity> toEntities(List<Country> dtos);
